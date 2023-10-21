7977b08c2ea2106e643653716ed4b31905fbd16c
Ashton Dunderdale <121163457+ashtondunderdale@users.noreply.github.com>
2023-10-21 12:24:00 +0100

    final config transactions

--- transactions.js
+++ transactions.js
@@ -2,17 +2,18 @@ document.addEventListener('DOMContentLoaded', function() {
     const transactionType = document.getElementById('transactionType');
     const transactionAmount = document.getElementById('transactionAmount');
     const transactionReference = document.getElementById('transactionReference');
-    const transactionCategory = document.getElementById('transactionCategory'); // Added
+    const transactionCategory = document.getElementById('transactionCategory'); 
     const addTransactionBtn = document.getElementById('addTransaction');
     const incomeTotalAmount = document.getElementById('incomeTotalAmount');
     const expenseTotalAmount = document.getElementById('expenseTotalAmount');
     const transactionList = document.querySelector('.transactionList');
+    const transactionMessage = document.getElementById('transactionMessage'); 
 
     addTransactionBtn.addEventListener('click', function() {
         const type = transactionType.value;
         const amount = parseFloat(transactionAmount.value);
         const reference = transactionReference.value;
-        const category = transactionCategory.value; // Added
+        const category = transactionCategory.value;
 
         if (!isNaN(amount)) {
             const transactionItem = document.createElement('div');
@@ -30,10 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
             } else if (type === 'expense') {
                 transactionItem.style.color = '#eb5e5e';
             }
-
-            incomeTotalAmount.textContent = `$${incomeTotal.toFixed(2)}`;
-            expenseTotalAmount.textContent = `$${expenseTotal.toFixed(2)}`;
-
+            
             transactionAmount.value = '';
             transactionReference.value = '';
         }
