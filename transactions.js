5e3f20ad36bdac138c661d35f5c43d3b993e2e33
Ashton Dunderdale <121163457+ashtondunderdale@users.noreply.github.com>
2023-10-23 19:10:02 +0100

    configured delete icon

--- transactions.js
+++ transactions.js
@@ -9,6 +9,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const transactionList = document.querySelector('.transactionList');
     const transactionMessage = document.getElementById('transactionMessage'); 
 
+    const savedTransactions = JSON.parse(localStorage.getItem('transactions')) || [];
+    savedTransactions.forEach(transaction => {
+        renderTransaction(transaction);
+    });
+
     addTransactionBtn.addEventListener('click', function() {
         const type = transactionType.value;
         const amount = parseFloat(transactionAmount.value);
@@ -16,28 +21,66 @@ document.addEventListener('DOMContentLoaded', function() {
         const category = transactionCategory.value;
 
         if (!isNaN(amount)) {
-            const transactionItem = document.createElement('div');
-            transactionItem.className = 'transactionItem';
-            transactionItem.textContent = `${category.charAt(0).toUpperCase()  + category.slice(1)} ${type.charAt(0).toUpperCase() + type.slice(1)}  - £${amount.toFixed(2)}`;
+            const transactionData = {
+                type,
+                amount,
+                reference,
+                category
+            };
 
-            if (reference) {
-                transactionItem.textContent += ` | ${reference}`;
-            }
+            savedTransactions.push(transactionData);
+            localStorage.setItem('transactions', JSON.stringify(savedTransactions));
 
-            transactionList.appendChild(transactionItem);
+            renderTransaction(transactionData);
 
-            if (type === 'income') {
-                transactionItem.style.color = '#4ceda5';
-            } else if (type === 'expense') {
-                transactionItem.style.color = '#eb5e5e';
-            }
-
-            if (transactionList.childElementCount > 0) {
-                transactionMessage.textContent = '';
-            }
-            
             transactionAmount.value = '';
             transactionReference.value = '';
         }
     });
+
+    function renderTransaction(transactionData) {
+        const { type, amount, reference, category } = transactionData;
+        const transactionItem = document.createElement('div');
+        transactionItem.className = 'transactionItem';
+    
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+        deleteButton.className = 'deleteButton';
+    
+        deleteButton.addEventListener('click', function() {
+            removeTransaction(transactionItem, transactionData);
+        });
+    
+        transactionItem.appendChild(deleteButton);
+    
+        const transactionText = document.createElement('span');
+        transactionText.textContent = `${category.charAt(0).toUpperCase() + category.slice(1)} ${type.charAt(0).toUpperCase() + type.slice(1)} - £${amount.toFixed(2)}`;
+    
+        if (reference) {
+            transactionText.textContent += ` | ${reference}`;
+        }
+    
+        transactionItem.appendChild(transactionText);
+    
+        transactionList.appendChild(transactionItem);
+    
+        if (type === 'income') {
+            transactionItem.style.color = '#4ceda5';
+        } else if (type === 'expense') {
+            transactionItem.style.color = '#eb5e5e';
+        }
+    
+        if (transactionList.childElementCount > 0) {
+            transactionMessage.textContent = '';
+        }
+    }    
+    
+    function removeTransaction(transactionItem, transactionData) {
+        const index = savedTransactions.indexOf(transactionData);
+        if (index !== -1) {
+            savedTransactions.splice(index, 1);
+            localStorage.setItem('transactions', JSON.stringify(savedTransactions));
+        }
+        transactionList.removeChild(transactionItem); 
+    }
 });
